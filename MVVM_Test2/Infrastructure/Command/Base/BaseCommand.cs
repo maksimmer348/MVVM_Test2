ac914b5a4cf46a087b5627c0fd75275bc1ea4cc6
maksimmer348 <maksimmer348@gmail.com>
2022-04-01 17:02:07 +0300

    добавление в труктуру Команд и класса BaseComand

---
+++ MVVM_Test2/Infrastructure/Command/Base/BaseCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MVVM_Test2.Command.Base;
+
+public abstract class BaseCommand : ICommand
+{
+    //событие генериуется когда метод CanExecute возрващает другое значение те когда кмд преходит из одного
+    //состояни в другое true->false или false->true, или можно выполнять это событие какимто своим способом
+    public event EventHandler? CanExecuteChanged
+    {
+        //событие передает управленеие классом CommandManager когда чтот происходит
+        add => CommandManager.RequerySuggested += value;
+        remove =>CommandManager.RequerySuggested -= value;
+    }
+    
+    //если функция вернут false то команду выполнить нельзя то эл к которму привязана команда отключается
+    public abstract bool CanExecute(object? parameter);
+
+    //то что будет выполнено самой коммандой, те логика команды
+    public abstract void Execute(object? parameter);
+
+}
