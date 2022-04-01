76da5a12d8d64de29f700f66f0f54d5e63fdb348
maksimmer348 <maksimmer348@gmail.com>
2022-04-01 11:30:31 +0300

    VM главного окна, пример оформления свойства

---
+++ MVVM_Test2/VM/MainWindowVM.cs
@@ -0,0 +1,33 @@
+﻿namespace MVVM_Test2;
+
+/// <summary>
+/// VM основного окна 
+/// </summary>
+public class MainWindowVM : BaseVM
+{
+    //создаем свойство и подцепляем к нему визуальный эл
+
+    #region Заголовок окна
+
+    private string title;
+
+    /// <summary>
+    /// Заголовок окна
+    /// </summary>
+    public string Title
+    {
+        get => title;
+        // set
+        // {
+        //     //если значение свойста Title не меняется то мы ничего не делаем
+        //     if (Equals((title, value))) return;
+        //     title = value;
+        //     OnPropertyChanged();
+        // }
+
+        //тк в MainVM уже есть конструкция Set сокращзаем вышенаписаное до:
+        set => Set(ref title, value);
+    }
+
+    #endregion
+}
