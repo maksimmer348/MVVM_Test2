86fcb6aaa50cf1ab885b6978bd45d71d10e6f70d
maksimmer348 <maksimmer348@gmail.com>
2022-04-04 11:25:00 +0300

    Добавление обработчика команд, команды и привязка ее к кнопке

--- MVVM_Test2/VM/MainWindowVM.cs
+++ MVVM_Test2/VM/MainWindowVM.cs
@@ -1,10 +1,61 @@
-﻿namespace MVVM_Test2;
+﻿using System.Windows;
+using System.Windows.Input;
+using MVVM_Test2.Command;
+
+namespace MVVM_Test2;
 
 /// <summary>
 /// VM основного окна 
 /// </summary>
 public class MainWindowVM : BaseVM
 {
+    public MainWindowVM()
+    {
+
+        #region Команды
+        
+        CloseAppCmd = new ActionCommand(OnCloseAppCmdExecuted, CanCloseAppCmdExecuted);
+
+        #endregion
+    }
+
+    #region Команды
+
+    #region Команда закрытия окна
+
+    /// <summary>
+    /// Свойство команды закрытия окна (те сама команда)
+    /// </summary>
+    public ICommand CloseAppCmd { get;}
+
+    
+    /// <summary>
+    /// Выполнение логики команды
+    /// </summary>
+    /// <param name="p"></param>
+    void OnCloseAppCmdExecuted(object p)
+    {
+        Application.Current.Shutdown();
+    }
+    
+    /// <summary>
+    /// Доступность команды для выполнения
+    /// </summary>
+    /// <param name="p"></param>
+    /// <returns></returns>
+    bool CanCloseAppCmdExecuted(object p)
+    {
+        //команда доступна для выполнения всегда, поэтому возварщаем труе 
+        return true;
+    }
+
+
+    #endregion
+   
+    #endregion
+    
+    #region Свойства
+
     //создаем свойство и подцепляем к нему визуальный эл
 
     #region Заголовок окна
@@ -30,19 +81,20 @@ public class MainWindowVM : BaseVM
         set => Set(ref title, value);
     }
 
-private int status;
+    private int status;
 
-public int Status
-{
-    get => status;
-    set
+    public int Status
     {
-        Set(ref status, value);
-        if (status > 10) Title = "Не готов";
-        else Title = "Готов";
+        get => status;
+        set
+        {
+            Set(ref status, value);
+            if (status > 10) Title = "Не готов";
+            else Title = "Готов";
+        }
     }
-}
+
     #endregion
 
-  
+    #endregion
 }
